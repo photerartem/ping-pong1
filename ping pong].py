d8eff657e529a755c2ddda8ee730d33080878a73
photerartem <idiatullinartem28@gmail.com>
2025-05-23 18:31:37 +0300

    Add files via upload

--- ping pong].py
+++ ping pong].py
@@ -1,4 +1,21 @@
 from pygame import *
+from pygame.cursors import ball
+
+
+back = (200, 255, 255) 
+win_width = 600
+win_height = 500
+window = display.set_mode( (win_width, win_height)) 
+window.fill(back)
+
+game = True 
+finish - False 
+clock =time.Clock()
+FPS = 60
+
+
+img_ball = 'ball.png'
+img_rocetka = 'rocetka.png'
 
 
 
@@ -7,58 +24,52 @@ font1 = font.Font(None, 80)
 win = font1.render('win 2!', True, (255, 255, 255))
 lose = font1.render('win 1!', True, (180, 0, 0))
 
+class GameSprite():
+    def __init__(self, player_image, player_y, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 
 
-img_ball = 'ball.png'
-img_rocetka = 'rocetka.png'
-
-
-class Plaeyer(sprite.Sprite):
-
-
-class roketka(GameSprite):
+class Player(GameSprite):
 
     def update_l(self):
-        keys key.get_pressed()
+        keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y - self.speed
-        if keys[K_s] and self.rect.y > 5:
-self.rect.y += self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self,rect.y - self. speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y +- self.speed
 
+while game:
+    for e in event.get():
+        if e.type - QUIT:
+            game = False
 
+if finish != True:
+    window.fill(back)
+    racket1.update_l()
+    racket2.update_r()
 
-def update_r(self):
-keys = key.get_pressed()
-if keys[K_UP] and self.rect.y > 5:
-self,rect.y - self. speed
-if keys[K_D0WN] and solf.rect.y > 5:
-self.rect.y +- self.speed
+    racket1.reset()
+    rocetka2.reset()
+    ball.reset()
+    
+display.update()
+clock.tick(FPS)
 
-
-
-
-def update_l(self):
-    keys = key.get_pressed()
-    if keys[K_w] and self.rect.y > 5:
-        self.rect.y += self.speed
-
-def update_l(self):
-    keys = key.get_pressed()
-    if keys[K_s] and self.rect.y > 5:
-        self.rect.y -= self.speed
-
-
-def update_r(self):
-    keys = key.get_pressed()
-    if keys[K_UP] and self.rect.y > 5:
-        self.rect.y += self.speed
-
-def update_r(self):
-    keys = key.get_pressed()
-    if keys[K_DOWN] and self.rect.y > 5:
-        self.rect.y -= self.speed
 
 win_width = 700
 win_height = 500
@@ -68,15 +79,7 @@ Window.fill(back)
 rocetka = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
 
-  def __init__(self, player_image, player_y, size_y, player_speed):
-      sprite.Sprite.__init__(self)
-      self.image = transform.scale(image.load(player_image), (size_y))
-      self.speed = player_speed
-      self.rect = self.image.get_rect()
-      self.rect.y = player_y
-
-  def reset(self):
-      window.blit(self.image, (self.rect.x, self.rect.y))
+  
 
 
 
